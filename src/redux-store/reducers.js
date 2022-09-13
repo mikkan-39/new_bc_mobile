ea838befd083ef509169e612cfee98719a4ac4ab
Mikkan <egor.altsoft2@gmail.com>
2022-09-13 17:10:51 +0300

    preparing for redux-saga

--- src/redux-store/reducers.js
+++ src/redux-store/reducers.js
@@ -2,16 +2,10 @@ import reactotron from 'reactotron-react-native';
 import * as types from './constants'
 
 const initialState = {
-  authorized: false,
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case types.REQUEST_LOGIN:
-      return {
-        ...state,
-        authorized: true,
-      };
     default:
       // redux may have called it's own actions,
       // otherwise something weird happened
