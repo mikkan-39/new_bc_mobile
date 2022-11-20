a474c77d0f347c1bfdbc9e270c1c108e120f8602
mikkan-39 <egor.altsoft2@gmail.com>
2022-11-21 02:04:01 +0300

    Added beutiful glow to loaded tickets

--- src/components/tabScreen/tableElement.tsx
+++ src/components/tabScreen/tableElement.tsx
@@ -2,9 +2,11 @@ import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useCallback } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
+import { useSelector } from "react-redux";
 import { themeAwareStyles } from "../../configs/themeAwareHook";
 import { StyleStorage } from "../../configs/themesConstants";
 import { stripHTML, TicketForRequest } from "../../redux-store/interfaces";
+import { RootState } from "../../redux-store/store";
 
 interface Props {
   element: TicketForRequest; // MUST have ParentTable
@@ -18,6 +20,9 @@ export default function TableElement(props: Props) {
   const styleStorage = themeAwareStyles();
   const styles = styleStorage.tabScreen.tableElement as StyleStorage;
   let ticket = props.element;
+  const ticketIsLoaded = Boolean(
+    useSelector((state: RootState) => state.ticketStorage[ticket.Key])
+  );
   ticket.Name = stripHTML(ticket.Name);
 
   const navigation =
@@ -27,7 +32,11 @@ export default function TableElement(props: Props) {
   }, [props, navigation]);
 
   return (
-    <TouchableOpacity style={styles.container} onPress={navigateToUpdater}>
+    //TODO: show selected style only if ticket was edited
+    <TouchableOpacity
+      style={ticketIsLoaded ? styles.containerSelected : styles.container}
+      onPress={navigateToUpdater}
+    >
       <View style={styles.upperRow}>
         <Text style={styles.rightText}>{ticket.Right}</Text>
       </View>
